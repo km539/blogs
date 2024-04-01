b00c672c163e52b71bacd1f0ac24b201fe300e01
kyojiMatsushima <kyoji.matsushima@asterone.co.jp>
2024-04-01 13:10:45 +0900

    バックエンドAPIからデータを取得するロジック作成。

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,81 @@
+import React, { useEffect, useState } from "react";
+
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const res = await fetch("/api/comments");
+    const jsonData = await res.json();
+    setData(jsonData.comments);
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <div class="replyBox" hidden>
+        <form class="replyForm">
+          <textarea
+            name="replyComment"
+            id=""
+            cols="50"
+            rows="3"
+            required
+          ></textarea>
+          <button type="submit">Reply</button>
+        </form>
+      </div>
+      {data.map(com => {
+        return (
+          <div className={`comment${com.id}`} key={com.id}>
+            <textarea readOnly rows={4} cols={50}>
+              {com.content}
+            </textarea>
+            <div>
+              <p>{com.user.username}</p>
+              <p>{com.createdAt}</p>
+              <button type="button" className="replayBtn">
+                reply
+              </button>
+            </div>
+            <p>{com.score}</p>
+            <div className="replies">
+              {com.replies.map(row => {
+                return (
+                  <div className={`comment${row.id}`}>
+                    <textarea readOnly rows={4} cols={50}>
+                      {row.content}
+                    </textarea>
+                    <div>
+                      <p>{row.user.username}</p>
+                      <p>{row.createdAt}</p>
+                      <button type="button" className="replayBtn">
+                        reply
+                      </button>
+                    </div>
+                    <p>{row.score}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <div className="post">
+        <form className="postForm" onSubmit={() => {}}>
+          <textarea
+            name="comment"
+            value={""}
+            onChange={() => {}}
+            cols="50"
+            rows="3"
+            required
+          ></textarea>
+          <button type="submit">Post</button>
+        </form>
+      </div>
     </>
   );
 };
