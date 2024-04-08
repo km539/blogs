5ee543d5e96f84d42f6ff2d8c1223808323270e2
kyojiMatsushima <kyoji.matsushima@asterone.co.jp>
2024-04-08 18:50:33 +0900

    Post.js作成。

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,32 @@
 import React, { useEffect, useState } from "react";
-import Comment from "./Components/Comment";
 import "./App.css";
+import Comment from "./Components/Comment";
+import Post from "./Components/Post";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [comment, setComment] = useState("");
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const res = await fetch("/api/comments");
+    // const res = await fetch("/api/comments");
+    const res = await fetch("http://localhost:5000/api/comments");
     const jsonData = await res.json();
     setData(jsonData.comments);
-  };
-
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
   };
 
   return (
     <>
       <div className="comments">
-        {data.map((comment) => (
+        {data.map(comment => (
           <div key={comment.id}>
             <Comment comment={comment} />
           </div>
         ))}
       </div>
-      <div className="post">
-        <form className="postForm" onSubmit={() => {}}>
-          <textarea
-            name="comment"
-            value={comment}
-            onChange={handleCommentChange}
-            cols="50"
-            rows="3"
-            required
-          ></textarea>
-          <button type="submit">Post</button>
-        </form>
-      </div>
+      <Post />
     </>
   );
 };
---
+++ frontend/src/Components/Post.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../styles/Post.css";
+const Post = () => {
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = e => {
+    setComment(e.target.value);
+  };
+
+  return (
+    <div className="post">
+      <form className="postForm" onSubmit={() => {}}>
+        <textarea
+          name="comment"
+          value={comment}
+          onChange={handleCommentChange}
+          cols="50"
+          rows="3"
+          required
+        ></textarea>
+        <button>Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default Post;
