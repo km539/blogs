1a40daf55f52faa8e1f0a7af04994ca8aa1f958c
kyojiMatsushima <kyoji.matsushima@asterone.co.jp>
2024-04-10 10:01:53 +0900

    データベースのセットアップ

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const knex = require("../lib/setUp.js");
 
 app.use(cors());
 app.use(express.json());
