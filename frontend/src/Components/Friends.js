05687c505cd7debbc8689cf2e4c08c17ebdcac46
EaintMoh <mohmoh@asterone.co.jp>
2024-06-10 15:43:08 +0900

    FriendsページのGUIの作成について修正して、styleも作成しました。

--- frontend/src/Components/Friends.js
+++ frontend/src/Components/Friends.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "../Styles/Friends.css";
 
 const Friends = () => {
   return (
-    <div className="Friends">
-      <h2>Friends</h2>
+    <div className="friends-container">
+      <div className="friends-list">
+        <h2>友達リスト</h2>
+        <p>人数：3名</p>
+        <div className="friend">
+          <img src="https://5.imimg.com/data5/SELLER/Default/2020/12/AP/QI/NQ/14784178/groot1.jpg" alt="友達の画像" />
+          <p>mo mo</p>
+        </div>
+        <div className="friend">
+          <img src="https://5.imimg.com/data5/SELLER/Default/2020/12/AP/QI/NQ/14784178/groot1.jpg" alt="友達の画像" />
+          <p>Du Du</p>
+        </div>
+        <div className="friend">
+          <img src="https://5.imimg.com/data5/SELLER/Default/2020/12/AP/QI/NQ/14784178/groot1.jpg" alt="友達の画像" />
+          <p>Bu Bu</p>
+        </div>
+      </div>
     </div>
   );
 };
